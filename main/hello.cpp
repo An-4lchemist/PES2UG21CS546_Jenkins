5c0b1b6142f2584ad6ec8bbffe81d0af621c5257
unknown <4suhask@gmail.com>
2024-03-07 21:45:44 +0530

    PES2UG21CS546 - Commiting jenkins main folder for lab

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  cout << "This is proof of me being PES2UG21CS546 :D\n";
+  return 0;
+}
